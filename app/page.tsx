41a490f960da436ce64b3119285fee077a01cefa
Neil John <neiljohnselencio@gmail.com>
2026-01-10 11:48:47 +0800

    update message

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[2px] row-start-2 items-center sm:items-start">
         Welcome to my app!
-        This is still on going...
+        This is an on going project...
 
         <div className="flex justify-center align-center">
           <button className="text-3xl">
